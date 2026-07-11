03f0209f4e9946b56a9d494f53f25ffbe42e2795
The412Banner <205237651+The412Banner@users.noreply.github.com>
2026-07-11 17:33:12 -0400

    fix(accounts): import layout.size in AccountAvatar (compile fix)

--- app/src/main/java/com/winlator/star/ui/AccountAvatar.kt
+++ app/src/main/java/com/winlator/star/ui/AccountAvatar.kt
@@ -1,6 +1,7 @@
 package com.winlator.star.ui
 
 import android.content.Context
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
@@ -16,7 +17,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.winlator.star.communityconfigs.AccountManager
 
